4552ea1be4a37f975012e158a18fb587117c3690
alvittoag <alvittogenz09@gmail.com>
2023-02-26 03:29:41 +0700

    update and add pinjam buku

--- src/pages/user/DetailSinopsis.tsx
+++ src/pages/user/DetailSinopsis.tsx
@@ -1,8 +1,28 @@
-import Base from "../../components/detail/Base";
+import { useAppSelector } from "../../app/hooks";
+import { RootState } from "../../app/store";
+import KonfirmasiPinjaman from "../../components/user/PinjamBuku/KonfirmasiPinjaman";
+import Base from "../../components/user/Sinopsis/Base";
 import assets from "../../constant/assets";
 
 const DetailSinopsis = () => {
-  return <Base />;
+  const togglePinjam = useAppSelector(
+    (state: RootState) => state.toogle.pinjam
+  );
+  return (
+    <div className="relative">
+      <Base />
+      {togglePinjam ? (
+        <div
+          className="absolute top-0 bg-black/40  -mt-24 h-[1210px]
+       right-0 left-0 bottom-0  "
+        >
+          <KonfirmasiPinjaman />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default DetailSinopsis;
